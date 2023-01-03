8528cd2757c3f649538b99ad343c1f0ee2386845
Eduard Hodun <eduard.hodun@gmail.com>
2023-01-03 10:54:17 +0200

    add HW 7-8

--- js/task-07.js
+++ js/task-07.js
@@ -3,3 +3,29 @@
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
+
+const inputRef = document.querySelector('#font-size-control');
+const spanRef = document.querySelector('#text');
+const pRef = document.querySelector('#text-p');
+
+
+// console.log(inputRef.value);
+// console.log(spanRef.textContent);
+// console.log( spanRef.style.fontSize);
+
+
+inputRef.addEventListener("input", onFontSizeControlClick);
+
+function onFontSizeControlClick(evt) { 
+
+//    console.log("event: ", evt.target);
+    
+    
+    spanRef.style.fontSize = `${inputRef.value}px`;
+    console.log(inputRef.value);
+
+
+    pRef.innerHTML = inputRef.value;
+
+
+};
--- js/task-08.js
+++ js/task-08.js
@@ -17,3 +17,76 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const formRef = document.querySelector('.login-form');
+// console.log(formRef);
+
+formRef.addEventListener("submit", OnLoginFormBtnClick);
+
+let formEmailPassword = {
+
+    email: '',
+    password: '',
+
+};
+
+function OnLoginFormBtnClick(evt) {
+
+    
+    
+    // ДОСТУП через властивість Elements
+
+    evt.preventDefault();
+
+    console.dir(evt.currentTarget.elements.email.value.length);
+
+    
+//     const {elements: { email, password } } = evt.currentTarget;
+
+
+
+
+//     if (email.value === "" || password.value === "") {
+
+//     return alert("Всі поля повинні бути заповнені!");
+//   }
+
+//     console.log(email.value, password.value);
+
+
+//     formEmailPassword.email = email.value;
+//     formEmailPassword.password = password.value;
+//     console.log(formEmailPassword);
+
+//     evt.currentTarget.reset();
+
+
+        // ДОСТУП через new FormData() - масив по бажанню, краще відправляти на сервер готову formData
+
+    
+       
+    
+    const formData = new FormData(evt.currentTarget);
+    console.log(formData);
+
+
+    const formEmailPasswordArray = [];
+
+    formData.forEach((value, name) => {
+
+        console.log('OnFormSubmit -> name', name);
+        console.log('OnFormSubmit -> value', value);
+
+        formEmailPasswordArray.push(name);
+        formEmailPasswordArray.push(value);
+
+        evt.currentTarget.reset();
+
+
+    });
+
+        console.log(formEmailPasswordArray);
+
+
+
+};
